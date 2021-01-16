fc33d9f445c9cc651076b58489a6f4a05b4f38dd
sitek94 <msitkowski94@gmail.com>
2021-01-16 19:04:22 +0100

    list shows the percentage when budget is passed

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,12 +1,26 @@
 import { BsXCircle as CrossIcon } from 'react-icons/bs';
 import { TransactionType } from 'types';
+import { percentFormat } from 'utils';
 import './list.scss';
 
 type Props = {
   list: TransactionType[];
+  budget?: number;
 };
 
-function List({ list }: Props) {
+function List({ list, budget }: Props) {
+
+  /**
+   * If there is `budget` prop it will calculate the percentage
+   * and render it.
+   */
+  const renderPercentage = (value: number) =>
+    budget !== undefined ? (
+      <div className="percentage">
+        {budget === 0 ? '---' : percentFormat(value, budget)}
+      </div>
+    ) : null;
+
   return (
     <ul className="list">
       {list.map(({ description, value }) => (
@@ -14,7 +28,7 @@ function List({ list }: Props) {
           <div>{description}</div>
           <div className="right-box">
             <div className="value">{value}</div>
-            <div className="percentage">20%</div>
+            {renderPercentage(value)}
             <button className="btn delete-btn">
               <CrossIcon />
             </button>
